1c6f31836e9009fc9e6f8d1a69c65c397dbe94ab
Sean Bartholomew <bartmnz@gmail.com>
2016-06-11 15:15:33 -0400

    added reset function

---
+++ reset.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+
+import psycopg2
+import sys
+import getpass
+
+def eprint(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
+
+def main():
+	if not (len(sys.argv) == 2):
+		eprint("ERROR: usage reset <database_name>")
+		sys.exit()
+	
+	try:
+		u_name = getpass.getuser()
+		connect_line = "dbname=" + str(sys.argv[1]) + " user=" + u_name
+		con = psycopg2.connect(connect_line)
+		cursor = con.cursor()
+	except:
+		eprint("ERROR: could not connect to database " + str(sys.argv[1]))
+	cursor.execute('DELETE from fight')
+	con.commit()
+	con.close()
+	
+
+
+if __name__ == "__main__":
+	main()
