e7590339b22bc3330f00f79520f2756f0c4cfc90
Sean Bartholomew <bartmnz@gmail.com>
2016-06-11 17:03:52 -0400

    pep-8

--- reset.py
+++ reset.py
@@ -5,27 +5,27 @@ import sys
 import getpass
 from make_dudes import eprint
 
+
 def eprint(*args, **kwargs):
-	print(*args, file=sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
+
 
 def main():
-	if not (len(sys.argv) == 2):
-		eprint("ERROR: usage reset <database_name>")
-		sys.exit()
-	
-	try:
-		u_name = getpass.getuser()
-		connect_line = "dbname=" + str(sys.argv[1]) + " user=" + u_name
-		con = psycopg2.connect(connect_line)
-		cursor = con.cursor()
-	except:
-		eprint("ERROR: could not connect to database " + str(sys.argv[1]))
-		sys.exit()
-	cursor.execute('DELETE from fight')
-	con.commit()
-	con.close()
-	
+    if not (len(sys.argv) == 2):
+        eprint("ERROR: usage reset <database_name>")
+        sys.exit()
+    try:
+        u_name = getpass.getuser()
+        connect_line = "dbname=" + str(sys.argv[1]) + " user=" + u_name
+        con = psycopg2.connect(connect_line)
+        cursor = con.cursor()
+    except:
+        eprint("ERROR: could not connect to database " + str(sys.argv[1]))
+        sys.exit()
+    cursor.execute('DELETE from fight')
+    con.commit()
+    con.close()
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- thunderdome.py
+++ thunderdome.py
@@ -6,35 +6,32 @@ from make_dudes import eprint
 import make_dudes as md
 
 
-
-
-
 def main():
     if not(len(sys.argv) == 2):
         print(str(len(sys.argv)))
         eprint("ERROR: usage stats <database_name>")
         sys.exit()
-    
-    #connect to database
+
+    # connect to database
     try:
-    	con, cursor = md.connect(sys.argv[1])
+        con, cursor = md.connect(sys.argv[1])
     except:
-        eprint("ERROR: could not connect to database "+ str(sys.argv[1]))
+        eprint("ERROR: could not connect to database " + str(sys.argv[1]))
         sys.exit()
-    #populate list of players
+    # populate list of players
     dude_list = md.DudeList(con, cursor)
-    if dude_list == None:
-    	con.close()
-    	sys.exit()
-    
-    #make sure fight data is empty ...annnnddd FIIIIIGGGHT
+    if dude_list is None:
+        con.close()
+        sys.exit()
+
+    # make sure fight data is empty ...annnnddd FIIIIIGGGHT
     dude_list.check_fight()
     dude_list.ultimate_showdown_of_ultimate_destiny()
 
-	#cleanup
+    # cleanup
     con.commit()
     con.close()
-    
+
 
 if __name__ == "__main__":
     main()
